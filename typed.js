b9fdcddd9be371fae77a9ed2a02cd8e8c0b2defb
Usama Ahmed <usamaahmed212aaa@gmail.com>
2022-01-02 09:50:00 +0600

    Smooth Typed.js Useing jQuery

--- typed.js
+++ typed.js
@@ -1,163 +1,94 @@
-"use strict";
-function Typed() {
-    document.querySelectorAll('.typed').forEach( function (element) {
-        const attributes = Array.from(element.querySelector('.typed-texts').attributes);
-        const attributesObj = {};
-        attributes.forEach(function (attribute) {
-            let name = attribute.name;
-            let value = attribute.value;
-            if( name.startsWith(/typed-[a-z][a-z]*/i.exec(name)) && (name.endsWith(/-[a-z][a-z]*/i.exec(name)) || name.endsWith(/-[a-z][a-z]*-[a-z]*/i.exec(name))) && (!value == '') && value == /[a-z0-9]*/i.exec(value) ) {
-                name = name.replace(/typed-/i, '').replace(/-([a-z])/gi, i => i.slice(1).toUpperCase());
-                attributesObj[name] = (value == /[0-9]*/.exec(value)) ? parseInt(value) : value;
-            }
+$(window).load(function () {
+    "use strict";
+    function Typed() {
+        $('.typed').each(function() {
+            const element = $(this);
+            const attributes = Array.from((element.context || element[0]).querySelector('.typed-texts').attributes);
+            const attributesObj = {};
+            attributes.forEach(function (attribute) {
+                let name = attribute.name;
+                let value = attribute.value;
+                
+                if( name.startsWith(/typed-[a-z][a-z]*/i.exec(name)) && (name.endsWith(/-[a-z][a-z]*/i.exec(name)) || name.endsWith(/-[a-z][a-z]*-[a-z]*/i.exec(name))) && (!value == '') && value == /[a-z0-9]*/i.exec(value) ) {
+                    name = name.replace(/typed-/i, '').replace(/-([a-z])/gi, i => i.slice(1).toUpperCase());
+                    attributesObj[name] = (value == /[0-9]*/.exec(value)) ? parseInt(value) : value;
+                }
+            });
+
+            typed(element,attributesObj);
         });
 
-        typed(element, attributesObj);
-    });
-
-    function typed(element, attributes) {
-        const {
-                startPoint = 'end',
+        function typed($word,attributes) {
+            const {
                 startDelay = 2000,
-                typeSpeed  = 0,
-                backDelay  = 1500,
-                backSpeed  = 0,
-                nextDelay  = 50,
+                typeSpeed = 600,
+                backDelay = 1500,
+                backSpeed = 600,
+                nextDelay  = 0,
                 showCursor = 'true',
                 cursorBlink = 'true',
-                inEffect   = '',
-                outEffect  = '',
-        } = attributes;
+            } = attributes;
 
-        const typedTexts = element.querySelector('.typed-texts');
+            if (showCursor == 'false') {
+                $word.find('.typed-texts').eq(0).addClass('cursor-hide');
+            }
+    
+            if (showCursor == 'true' && cursorBlink == 'true') {
+                $word.find('.typed-texts').eq(0).addClass('blink');
+            }
 
-        if (showCursor == 'false') {
-            typedTexts.classList.add('cursor-hide');
-        }
+            if (!$word.find('.typed-texts span').eq(0).hasClass('type')) {
+                $word.find('.typed-texts span').eq(0).addClass('type');
+            }
 
-        if (showCursor == 'true' && cursorBlink == 'true') {
-            typedTexts.classList.add('blink');
-        }
+            const spanWrapper = $word.find('.typed-texts');
+            const newWidth = spanWrapper.width() + 10;
+            spanWrapper.css('width', newWidth);
 
-        if (!element.querySelector('.typed-texts span.type')) {
-            element.querySelector('.typed-texts span').classList.add('type');
-        }
+            //trigger animation
+            setTimeout(function() {
+                hideWord($word.find('.type').eq(0));
+            }, startDelay);
 
-        const typedTextsWidth = element.querySelector('.typed-texts span.type').scrollWidth + 10;
-        (startPoint == 'end') ? typedTexts.style.width = typedTextsWidth +'px' : typedTexts.style.width = 0 +'px'; 
-        
-        const typeStart = setInterval(() => {
-            (startPoint == 'end') ? erase() : type();
-            clearInterval(typeStart);
-        }, startDelay);
+            function hideWord($word) {
+                var nextWord = takeNext($word);
+                $word.parents('.typed-texts').animate({
+                    width: '1px'
+                }, backSpeed, function() {
+                    $word.addClass('hide');
 
-        function type () {
-            const typedTextsWidth = element.querySelector('.typed-texts span.type').scrollWidth + 10;
-            let width = 0;
-            const type = setInterval(() => {
-                if (inEffect == 'clipIn') {
-                    let percent = Math.floor((width / typedTextsWidth) * 100);
-                    if (percent <= 30) {
-                        width += 1.40;
-                    } else if (percent <= 35) {
-                        width += 1.35;
-                    } else if (percent <= 40) {
-                        width += 1.30;
-                    } else if (percent <= 45) {
-                        width += 1.25;
-                    } else if (percent <= 50) {
-                        width += 1.20;
-                    } else if (percent <= 55) {
-                        width += 1.15;
-                    } else if (percent <= 60) {
-                        width += 1.10;
-                    }  else if (percent <= 65) {
-                        width += 1.05;
-                    } else {
-                        width += 1;
-                    }
-                } else {
-                    width += 1;
-                }
-
-                if (typedTextsWidth >= width) {
-                    typedTexts.style.width = width +'px';
-                } else {
-                    clearInterval(type);
-                    setTimeout(() => {
-                        erase();
-                    }, backDelay);
-                }
-            }, typeSpeed);
-        }
-
-        function erase () {
-            const typedTextsWidth = element.querySelector('.typed-texts span.type').scrollWidth + 10;
-            let width = typedTextsWidth;
-            const erase = setInterval(() => {
-                if (outEffect == 'clipOut') {
-                    let percent = Math.floor((width / typedTextsWidth) * 100);
-                    if (percent >= 70) {
-                        width -= 1.40;
-                    } else if (percent >= 65) {
-                        width -= 1.35;
-                    } else if (percent >= 60) {
-                        width -= 1.30;
-                    } else if (percent >= 55) {
-                        width -= 1.25;
-                    } else if (percent >= 50) {
-                        width -= 1.20;
-                    } else if (percent >= 45) {
-                        width -= 1.15;
-                    } else if (percent <= 40) {
-                        width -= 1.10;
-                    } else if (percent <= 35) {
-                        width -= 1.05;
-                    } else {
-                        width -= 1;
-                    }
-                }  else {
-                    width -= 1;
-                }
-
-                if (width > 0) {
-                    typedTexts.style.width = width +'px';
-                } else {
-                    clearInterval(erase);
-                    const typedText = element.querySelector('.typed-texts span.type');
-
-                    typedText.classList.add("hide");
-
-                    setTimeout(() => {
-                        takeNext();
-                        type();
+                    setTimeout(function() {
+                        switchWord($word, nextWord);
+                        showWord(nextWord);
                     }, nextDelay);
-                }
-            }, backSpeed);
-        }
-        
-        function takeNext () {
-            const typedTexts = element.querySelectorAll('.typed-texts span');
-            const typedText = element.querySelector('.typed-texts span.type');
+                });
+            }
 
-            typedText.classList.remove('hide');
+            function showWord($word) {
+                $word.parents('.typed-texts').animate({
+                'width': $word.width() + 10
+                }, typeSpeed, function() {
+                    setTimeout(function() {
+                        hideWord($word)
+                        }, backDelay);
+                });
+            }
+
+            function takeNext($word) {
+                return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+            }
             
-            if (typedText.nextElementSibling) {
-                typedText.classList.remove('type');
-                if (typedText.getAttribute('class') == '') {
-                    typedText.removeAttribute('class');
+            function switchWord($oldWord, $newWord) {
+                $oldWord.removeClass('hide');
+
+                $oldWord.removeClass('type');
+                if ($oldWord.attr('class') == '') {
+                    $oldWord.removeAttr('class');
                 }
-                typedText.nextElementSibling.classList.add('type');
-            } else {
-                typedText.classList.remove('type');
-                if (typedText.getAttribute('class') == '') {
-                    typedText.removeAttribute('class');
-                }
-                typedTexts[0].classList.add('type');
+                $newWord.addClass('type');
             }
         }
-
     }
-}
 
-Typed();
+    Typed();
+});
